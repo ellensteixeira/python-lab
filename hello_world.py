e0af055ff46f3cbd76262ce84c43f2f8ad237ed2
wwwellen <ellen.http4@gmail.com>
2025-02-11 21:55:36 -0300

    add o tradicional primeiro código

---
+++ hello_world.py
@@ -0,0 +1,6 @@
+# 1º código, o tradicional hello world.
+
+print('Hello World')
+
+# print - é uma função que imprime/exibe uma resposta na tela para o usuário.
+# os parênteses () indicam que é uma função.
