357accd3fb85ee6d79ba3cb2ba0c49b851610dd8
wwwellen <ellen.http4@gmail.com>
2025-02-24 19:04:37 -0300

    updt operadores aritméticos

--- operadores.py
+++ operadores.py
@@ -7,3 +7,15 @@ print(6 * 7) #multiplicação
 print(84 / 2) #divisão
 
 print(6 ** 2) #exponenciação
+
+# usando variavéis para imprimir os resultados:
+
+soma =  5 + 10
+
+subt = 15 - 5
+
+mult = 7 * 3
+
+divisao = 16 / 2
+
+print(soma, subt, mult, divisao)
